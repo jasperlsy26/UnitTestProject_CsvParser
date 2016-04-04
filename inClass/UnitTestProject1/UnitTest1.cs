543ab986b313359e8778089f6433af4211660641
Siyang Li <jasperlsy26@gmail.com>
2016-04-04 14:28:59 -0700

    UnitTestCsvParser

---
+++ inClass/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using inClass;
+/*
+ * Csv is a spreadsheet format
+ * Csv stands for comma seperated values
+ * Formaet lookds like
+ * row0col0,row0col1rawocol2
+ * row1col0,row1col1,row1col2
+ * */
+using UnitTestProject1;
+
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CsvUnitTest
+    {
+        [TestMethod]
+        public void ParseSimpleRowTest()
+        {
+            CsvParser reader = new CsvParser();
+            var result = reader.ParseRow("one,two");
+
+            // from the above, we expect two entries
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual("one", result[0]);
+            Assert.AreEqual("two", result[1]);
+            // how to check cover result?
+        }
+
+        [TestMethod]
+        public void EmptySimpleRowTest()
+        {
+            CsvParser parser = new CsvParser();
+            var result = parser.ParseRow(",,,");
+            Assert.AreEqual(4, result.Count);
+            Assert.AreEqual("", result[0]);
+            Assert.AreEqual("", result[1]);
+        }
+
+        [TestMethod]
+        public void EmbeddedCommaTest()
+        {
+            CsvParser parser = new CsvParser();
+            var result = parser.ParseRow("\"Smith, Bob\",A");
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual("Smith, Bob", result[0]);
+            Assert.AreEqual("A", result[1]);
+        }
+
+        [TestMethod]
+        public void EmbeddedQuoteTest()
+        {
+            CsvParser parser = new CsvParser();
+            var result = parser.ParseRow("\"\"\"H\"\"ello\",A");
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual("\"H\"ello", result[0]);
+            Assert.AreEqual("A", result[1]);
+        }
+
+        [TestMethod]
+        public void EmbeddedQuoteAndCommaTest()
+        {
+            CsvParser parser = new CsvParser();
+            var result = parser.ParseRow("\"\"\"H\"\"ello\",A");
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual("\"H\"ello", result[0]);
+            Assert.AreEqual("A", result[1]);
+        }
+    }
+}
---
+++ inClass/inClass/CsvParser.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text.RegularExpressions;
+
+
+/*
+ * Good for logic testing
+ * For UI based project, it is not veru appropriate.
+ * 
+ * */
+namespace UnitTestProject1
+{
+    public class CsvParser
+    {
+        /// <summary>
+        /// Parsing rule: 
+        ///     1. A cell may or may not start with a single quote
+        ///     2. If a cell starts with a quote, it must end with a quote
+        ///     3. If a quote exists as a cell component , the cell must start with quote
+        ///     4. quotes within a cell are double escaped  r.g ""
+        ///     5. if a commma exists as a cell component, the cell must start and end with quotes
+        ///     
+        ///     
+        /// </summary>
+        /// <param name="row"></param>
+        /// <returns></returns>
+        public List<String> ParseRow(string row)
+        {
+            // simple row parsing
+
+            //string[] tmp;
+            //tmp = row.Split(',');
+            //return tmp.ToList();
+
+
+            List<string> retList = new List<string>();
+            string at = "";
+            for (int iter = 0; iter < row.Length; iter++)
+            {
+                if (row[iter] == '"')
+                {
+                    iter++;
+                    if (row[iter] == '"')
+                    {
+                        at += row[iter];
+                        iter++;
+                    }
+                    while (iter < row.Length && row[iter] != '"')
+                    {
+                        at += row[iter];
+                        iter++;
+                        if ((iter < row.Length - 1) 
+                            && (row[iter] == '"') 
+                            && (row[iter + 1] == '"'))
+                        {
+                            at += row[iter];
+                            iter += 2;
+                        }
+                    }
+                }
+                else if (row[iter] == ',')
+                {
+                    retList.Add(at);
+                    at = "";
+                }
+                else
+                {
+                    at += row[iter];
+                }
+            }
+            retList.Add(at);
+            return retList;
+        }
+
+
+
+        public List<String> ParseRowReg(string row)
+        {
+            // Decent method using regular expression
+            // source: http://stackoverflow.com/questions/18757097/writing-data-into-csv-file
+            Regex regex = new Regex(@"^(?:""(?<item>[^""]*)""|(?<item>[^,]*))(?:,(?:""(?<item>[^""]*)""|(?<item>[^,]*)))*$");
+            List<String> list = regex
+              .Match(row)
+              .Groups["item"]
+              .Captures
+              .Cast<Capture>()
+              .Select(c => c.Value)
+              .ToList();
+
+            return list;
+        }
+
+
+
+    }
+}
